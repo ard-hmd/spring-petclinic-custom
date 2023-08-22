df85da4773b1436e96791caf3d8f3b424dc7e842
EC2 Default User <ec2-user@ip-172-31-11-70.eu-west-3.compute.internal>
2023-08-22 10:08:24 +0000

    update script to push only the usefull images

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -3,7 +3,7 @@ docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
+#docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
+#docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
+#docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
 
