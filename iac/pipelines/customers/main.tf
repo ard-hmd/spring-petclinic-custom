868cf6ae591aa24c4e431c132bff7c95e2d41d1f
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-15 22:19:05 +0200

    remove environment variable from terraform and relying on the buildspec.yml ones

--- iac/pipelines/customers/main.tf
+++ iac/pipelines/customers/main.tf
@@ -12,39 +12,6 @@ resource "aws_codebuild_project" "pipeline" {
         image                       = "aws/codebuild/amazonlinux2-x86_64-standard:corretto11-23.07.28"
         type                        = "LINUX_CONTAINER"
         image_pull_credentials_type = "CODEBUILD"
-
-        environment_variable {
-            name  = "CLUSTER_NAME"
-            value = "PETCLINIC_EKS_CLUSTER_NAME"
-            type = "PARAMETER_STORE"
-        }
-
-        environment_variable {
-            name  = "DOCKER_LOGIN"
-            value = "PETCLINIC_DOCKER_USERNAME"
-            type = "PARAMETER_STORE"
-        }
-
-        environment_variable {
-            name  = "DOCKER_PASSWORD"
-            value = "PETCLINIC_DOCKER_PASSWORD"
-            type = "PARAMETER_STORE"
-        }
-
-        environment_variable {
-            name  = "REPOSITORY_PREFIX"
-            value = "michelnguyenfr"
-        }
-
-        environment_variable {
-            name  = "REPOSITORY_URI"
-            value = "michelnguyenfr/spring-petclinic-cloud-customers-service"
-        }
-
-        environment_variable {
-            name  = "TAG"
-            value = "latest"
-        }
     }
 
     artifacts {
