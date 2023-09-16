9cf8a38fd03d4603f9dc6ac61503a86c87a701cd
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-17 00:07:42 +0200

    fixing outputs

--- iac/pipelines/outputs.tf
+++ iac/pipelines/outputs.tf
@@ -1,2 +1,5 @@
-output "project_identifier" { value = module.pipeline_customers.project_identifier }
+output "project_identifier" { value = module.codebuild_customers.id }
+output "project_identifier" { value = module.codebuild_vets.id }
+output "project_identifier" { value = module.codebuild_visits.id }
+output "project_identifier" { value = module.codebuild_api_gateway.id }
 output "build_role_arn" { value = module.role_build.build_role_arn }
