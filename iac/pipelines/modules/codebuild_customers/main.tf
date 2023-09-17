5b59d62f85fee97312b51fe8b265064cbfcd2190
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-17 23:24:11 +0200

    adding privileged_mode to support Docker image build

--- iac/pipelines/modules/codebuild_customers/main.tf
+++ iac/pipelines/modules/codebuild_customers/main.tf
@@ -6,12 +6,13 @@ resource "aws_codebuild_project" "pipeline" {
     }
 
     source_version = "refs/heads/master"
-    
+
     environment {
         compute_type                = "BUILD_GENERAL1_SMALL"
         image                       = "aws/codebuild/amazonlinux2-x86_64-standard:corretto11-23.07.28"
         type                        = "LINUX_CONTAINER"
         image_pull_credentials_type = "CODEBUILD"
+        privileged_mode             = true
     }
 
     artifacts {
--- iac/pipelines/modules/codebuild_visits/main.tf
+++ iac/pipelines/modules/codebuild_visits/main.tf
@@ -12,6 +12,7 @@ resource "aws_codebuild_project" "pipeline" {
         image                       = "aws/codebuild/amazonlinux2-x86_64-standard:corretto11-23.07.28"
         type                        = "LINUX_CONTAINER"
         image_pull_credentials_type = "CODEBUILD"
+        privileged_mode             = true
     }
 
     artifacts {
