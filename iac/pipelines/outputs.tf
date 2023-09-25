1231f02bdb4328e0f6e0c8305542f48902971b77
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-25 23:18:21 +0200

    removing output for non existing resource

--- iac/pipelines/outputs.tf
+++ iac/pipelines/outputs.tf
@@ -1,5 +1,2 @@
-output "id_customers" { value = module.codebuild_customers.id }
-output "id_vets" { value = module.codebuild_vets.id }
-output "id_visits" { value = module.codebuild_visits.id }
 output "id_api_gateway" { value = module.codebuild_api_gateway.id }
 output "build_role_arn" { value = module.roles.build_role_arn }
