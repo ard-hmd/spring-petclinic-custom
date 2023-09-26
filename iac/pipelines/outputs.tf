2ae9dc530341edf6ea6752b12fe6d68858425d85
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-26 11:29:22 +0200

    typo in output

--- iac/pipelines/outputs.tf
+++ iac/pipelines/outputs.tf
@@ -1,2 +1,2 @@
-output "id_build" { value = ["${module.codebuild_customers.id}"] }
+output "id_build" { value = ["${module.codebuild_build.id_build}"] }
 output "build_role_arn" { value = module.roles.build_role_arn }
